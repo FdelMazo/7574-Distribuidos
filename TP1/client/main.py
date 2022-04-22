a4c1a72b26225ba5a424dcdbcf4f7b1d2f3cc897
FdelMazo <FdelMazo@fi.uba.ar>
2022-04-22 17:13:08 -0300

    Remove duped code on client

--- TP1/client/main.py
+++ TP1/client/main.py
@@ -11,6 +11,17 @@ def main():
     server_alias = config["DEFAULT"]["server_alias"]
     server_port = int(config["DEFAULT"]["server_port"])
 
+    def send_query(query):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect((server_alias, server_port))
+
+        conn.send(f"{query}\n".encode("utf-8"))
+        response = conn.recv(1024).rstrip().decode("utf-8")
+        print(f"SEND {query}")
+        print(f"RECV {response}")
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
+
     stats = {
         # https://psutil.readthedocs.io/en/latest/
         "cpu_percent": (psutil.cpu_percent(interval=1), 15),
@@ -24,57 +35,23 @@ def main():
     # Let's set up some alerts before logging our metrics
     operations = ["AVG", "MAX"]
     for metric_id, aggregate_op in itertools.product(stats, operations):
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect((server_alias, server_port))
-
         limit = stats[metric_id][1]
         aggregate_secs = 5
-        query = f"NEW-ALERT {metric_id} {aggregate_op} {aggregate_secs} {limit}\n"
-        conn.send(query.encode("utf-8"))
-
-        response = conn.recv(1024).rstrip().decode("utf-8")
-        status_code = response.split()[0]
-        if status_code != "200":
-            print(f"Error while sending query {query}: {response}")
-
-        conn.shutdown(socket.SHUT_RDWR)
-        conn.close()
+        query = f"NEW-ALERT {metric_id} {aggregate_op} {aggregate_secs} {limit}"
+        send_query(query)
 
     # Tell the world about our stats!
     time.sleep(2)
     for (metric_id, metric_value) in rounded_stats.items():
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect((server_alias, server_port))
-
-        query = f"LOG {metric_id} {metric_value}\n"
-        conn.send(query.encode("utf-8"))
-
-        response = conn.recv(1024).rstrip().decode("utf-8")
-        status_code = response.split()[0]
-        if status_code != "200":
-            print(f"Error while sending log {query}: {response}")
-
-        conn.shutdown(socket.SHUT_RDWR)
-        conn.close()
+        query = f"LOG {metric_id} {metric_value}"
+        send_query(query)
 
     # We now have lots of metrics, let's wait for them to arrive and then query them
     time.sleep(2)
     for metric_id, aggregate_op in itertools.product(stats, operations):
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect((server_alias, server_port))
-
         aggregate_secs = 5
-        query = f"QUERY {metric_id} {aggregate_op} {aggregate_secs}\n"
-        conn.send(query.encode("utf-8"))
-
-        response = conn.recv(1024).rstrip().decode("utf-8")
-        print(response)
-        status_code = response.split()[0]
-        if status_code != "200":
-            print(f"Error while sending query {query}: {response}")
-
-        conn.shutdown(socket.SHUT_RDWR)
-        conn.close()
+        query = f"QUERY {metric_id} {aggregate_op} {aggregate_secs}"
+        send_query(query)
 
 
 if __name__ == "__main__":
